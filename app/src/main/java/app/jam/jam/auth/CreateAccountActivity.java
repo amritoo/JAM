fe23b0a1fa0d9eb7646b6e260ff68e09da3a9f8b
amritoo <tipu.amrito@gmail.com>
2020-08-23 19:45:48 +0600

    Fix CreateAccountActivity

--- app/src/main/java/app/jam/jam/auth/CreateAccountActivity.java
+++ app/src/main/java/app/jam/jam/auth/CreateAccountActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -11,12 +13,11 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -25,6 +26,8 @@ import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Objects;
+
 import app.jam.jam.Manager;
 import app.jam.jam.R;
 import app.jam.jam.data.Constants;
@@ -32,12 +35,14 @@ import app.jam.jam.data.User;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
+    /**
+     * Tag to use to {@link Log} messages
+     */
     private static final String TAG = "Create Account";
 
     private FirebaseAuth mAuth;
     private DatabaseReference mRootReference;
 
-    private MaterialToolbar mToolbar;
     private TextInputLayout mUsername, mEmail, mPassword;
     private MaterialButton createAccountButton;
 
@@ -54,23 +59,14 @@ public class CreateAccountActivity extends AppCompatActivity {
 
         // for initializing views from layout
         InitializeViews();
-
-        createAccountButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String username = mUsername.getEditText().getText().toString();
-                String email = mEmail.getEditText().getText().toString();
-                String password = mPassword.getEditText().getText().toString();
-                createNewAccount(username, email, password);
-            }
-        });
+        setListeners();
     }
 
     // for initializing views from layout
     private void InitializeViews() {
-        mUsername = findViewById(R.id.username_textInputLayout);
-        mEmail = findViewById(R.id.email_textInputLayout);
-        mPassword = findViewById(R.id.password_textInputLayout);
+        mUsername = findViewById(R.id.create_account_username_textInputLayout);
+        mEmail = findViewById(R.id.create_account_email_textInputLayout);
+        mPassword = findViewById(R.id.create_account_password_textInputLayout);
         createAccountButton = findViewById(R.id.create_account_button);
 
         mProgressDialog = new ProgressDialog(this);
@@ -78,13 +74,111 @@ public class CreateAccountActivity extends AppCompatActivity {
         mProgressDialog.setMessage(getString(R.string.message_wait));
         mProgressDialog.setCanceledOnTouchOutside(true);
 
-        mToolbar = findViewById(R.id.create_account_toolbar);
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        MaterialToolbar toolbar = findViewById(R.id.create_account_toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setActivityResult(null);
             }
         });
+    }
+
+    // all listeners for corresponding views
+    private void setListeners() {
+        Objects.requireNonNull(mUsername.getEditText())
+                .addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                        // Do nothing
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        mUsername.setErrorEnabled(false);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        // Do nothing
+                    }
+                });
+
+        Objects.requireNonNull(mEmail.getEditText())
+                .addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                        // Do nothing
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        mEmail.setErrorEnabled(false);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        // Do nothing
+                    }
+                });
+
+        Objects.requireNonNull(mPassword.getEditText())
+                .addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                        // Do nothing
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        mPassword.setErrorEnabled(false);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        // Do nothing
+                    }
+                });
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createAccount();
+            }
+        });
+    }
+
+    /**
+     * This method reads input data and validates them. If valid, then it
+     * initiates {@link CreateAccountActivity#createNewAccount(String, String, String)}
+     * using these data
+     */
+    private void createAccount() {
+        String username = Objects
+                .requireNonNull(mUsername.getEditText())
+                .getText().toString();
+        String email = Objects
+                .requireNonNull(mEmail.getEditText())
+                .getText().toString();
+        String password = Objects
+                .requireNonNull(mPassword.getEditText())
+                .getText().toString();
+
+        // validating parameter data
+        if (!Manager.isValidUsername(this, username)) {
+            mUsername.setError(getString(R.string.error_username));
+            mUsername.setErrorEnabled(true);
+            return;
+        } else if (!Manager.isValidEmail(email)) {
+            mEmail.setError(getString(R.string.error_create_email));
+            mEmail.setErrorEnabled(true);
+            return;
+        } else if (!Manager.isValidPassword(this, password)) {
+            mPassword.setError(getString(R.string.error_create_password));
+            mPassword.setErrorEnabled(true);
+            return;
+        }
+
+        createNewAccount(username, email, password);
     }
 
     /**
@@ -95,55 +189,23 @@ public class CreateAccountActivity extends AppCompatActivity {
      * @param password password of the account
      */
     private void createNewAccount(final String username, final String email, final String password) {
-        // validating parameter data
-        if (!Manager.isValidUsername(this, username)) {
-            mUsername.setError(getString(R.string.warning_username));
-            mUsername.setErrorEnabled(true);
-            return;
-        } else if (!Manager.isValidEmail(email)) {
-            mEmail.setError(getString(R.string.warning_create_email));
-            mEmail.setErrorEnabled(true);
-            return;
-        } else if (!Manager.isValidPassword(this, password)) {
-            mPassword.setError(getString(R.string.warning_create_password));
-            mPassword.setErrorEnabled(true);
-            return;
-        }
-
         mProgressDialog.show();
         // Create account with email and password
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                     @Override
                     public void onSuccess(AuthResult authResult) {
-                        Log.i(TAG, "createUserWithEmail: success");
-
-                        User user = new User();
-                        user.setUserName(username);
+                        Log.i(TAG, "createUserWithEmail:success");
 
                         FirebaseUser currentUser = mAuth.getCurrentUser();
                         if (currentUser != null) {
-                            // Update username
                             updateUsername(currentUser, username);
 
-                            //update user database
-                            String currentUserID = currentUser.getUid();
-                            mRootReference.child(Constants.ROOT_USERS).child(currentUserID)
-                                    .setValue(user)
-                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                        @Override
-                                        public void onSuccess(Void aVoid) {
-                                            Log.i(TAG, "Update successful");
-                                        }
-                                    })
-                                    .addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            Log.e(TAG, "Update failed: " + e);
-                                        }
-                                    });
+                            User user = new User();
+                            user.setUserName(username);
+                            // add user to database and add admin as contact
+                            addUserToDatabase(currentUser, user);
                         }
-
                         Toast.makeText(CreateAccountActivity.this, R.string.toast_create_account_success, Toast.LENGTH_SHORT).show();
                         setActivityResult(email);
                         mProgressDialog.dismiss();
@@ -152,8 +214,15 @@ public class CreateAccountActivity extends AppCompatActivity {
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "createUserWithEmail:failure", e);
-                        Toast.makeText(CreateAccountActivity.this, R.string.toast_create_account_failed, Toast.LENGTH_SHORT).show();
+                        Log.e(TAG, "createUserWithEmail:failure", e);
+                        Snackbar.make(mUsername, R.string.snackbar_create_account_failed, Snackbar.LENGTH_LONG)
+                                .setAction(R.string.button_text_retry, new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        createAccount();
+                                    }
+                                })
+                                .show();
                         mProgressDialog.dismiss();
                     }
                 });
@@ -171,14 +240,72 @@ public class CreateAccountActivity extends AppCompatActivity {
                 .build();
 
         user.updateProfile(profileChangeRequest)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Log.i(TAG, "User profile updated.");
-                        } else {
-                            Log.e(TAG, "User profile update failed!");
-                        }
+                    public void onSuccess(Void aVoid) {
+                        Log.i(TAG, "updateUsername:success");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "updateUsername:failure", e);
+                    }
+                });
+    }
+
+    private void addUserToDatabase(FirebaseUser currentUser, User user) {
+        //update user database
+        String currentUserID = currentUser.getUid();
+        mRootReference.child(Constants.ROOT_USERS).child(currentUserID)
+                .setValue(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.i(TAG, "addUserToDatabase:success");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "addUserToDatabase:failure" + e);
+                    }
+                });
+        addAdminContact(currentUserID);
+    }
+
+    /**
+     * This method adds the admin account to registered user's contact list.
+     */
+    private void addAdminContact(final String currentUserId) {
+        mRootReference.child(Constants.ROOT_CONTACTS)
+                .child(currentUserId).child(Constants.CHILD_ADMIN_USER_ID)
+                .child(Constants.CHILD_CONTACT).setValue(Constants.CHILD_CONTACT_SAVED)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.i(TAG, "addAdminContact:successStage1");
+                        mRootReference.child(Constants.ROOT_CONTACTS)
+                                .child(Constants.CHILD_ADMIN_USER_ID).child(currentUserId)
+                                .child(Constants.CHILD_CONTACT).setValue(Constants.CHILD_CONTACT_SAVED)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.i(TAG, "addAdminContact:successStage2");
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.e(TAG, "addAdminContact:failureStage2", e);
+                                    }
+                                });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "addAdminContact:failureStage1", e);
                     }
                 });
     }
