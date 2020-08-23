62c69e84e6d06fa3f592c72b5eeb5ddee60c9f4b
amritoo <tipu.amrito@gmail.com>
2020-08-23 22:10:56 +0600

    Fix OnlineActivity

--- app/src/main/java/app/jam/jam/online/OnlineActivity.java
+++ app/src/main/java/app/jam/jam/online/OnlineActivity.java
@@ -1,12 +1,11 @@
 package app.jam.jam.online;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -17,6 +16,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.textview.MaterialTextView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -27,15 +27,23 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.util.Objects;
+
+import app.jam.jam.AboutActivity;
 import app.jam.jam.R;
 import app.jam.jam.auth.LoginActivity;
 import app.jam.jam.data.Constants;
+import app.jam.jam.help.HelpActivity;
 import app.jam.jam.offline.OfflineActivity;
+import app.jam.jam.profile.ProfileActivity;
 import app.jam.jam.settings.SettingsActivity;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class OnlineActivity extends AppCompatActivity {
 
+    /**
+     * Tag to use to {@link Log} messages
+     */
     private static final String TAG = "Online";
 
     FirebaseAuth mAuth;
@@ -45,11 +53,8 @@ public class OnlineActivity extends AppCompatActivity {
     private MaterialToolbar mToolbar;
     private CircleImageView mCurrentUserImage;
     private MaterialTextView mTitleTextView;
-    private LinearLayout mContainer;
 
-    private ContactsFragment mContactsFragment;
-    private ExploreFragment mExploreFragment;
-
+    private Fragment mContactsFragment, mExploreFragment;
     private String mUserName, mUserImage;
 
     @Override
@@ -63,34 +68,47 @@ public class OnlineActivity extends AppCompatActivity {
             goToLogin();
         }
 
+        // for initializing views and setting actions
         initializeViews();
-        setTabLayoutAction();
-        setToolbarAction();
+        setActions();
 
+        getUserNameAndImage();
 
         mContactsFragment = ContactsFragment.newInstance();
         mExploreFragment = ExploreFragment.newInstance();
-
         replaceFragment(mContactsFragment);
+    }
 
+    /**
+     * This method retrieves user name and image uri from {@link FirebaseDatabase}
+     * and sets it to title and user image.
+     */
+    private void getUserNameAndImage() {
         FirebaseDatabase.getInstance().getReference()
                 .child(Constants.ROOT_USERS).child(mCurrentUser.getUid())
                 .addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         if (snapshot.exists()) {
-                            mUserName = snapshot.child(Constants.CHILD_USERNAME).getValue().toString();
+                            mUserName = Objects
+                                    .requireNonNull(snapshot.child(Constants.CHILD_USERNAME)
+                                            .getValue()).toString();
                             mTitleTextView.setText(mUserName);
-                            if(snapshot.hasChild(Constants.CHILD_USER_IMAGE)) {
-                                mUserImage = snapshot.child(Constants.CHILD_USER_IMAGE).getValue().toString();
+                            Toast.makeText(OnlineActivity.this, getString(R.string.toast_welcome_user, mUserName), Toast.LENGTH_SHORT).show();
+
+                            if (snapshot.hasChild(Constants.CHILD_USER_IMAGE)) {
+                                mUserImage = Objects
+                                        .requireNonNull(snapshot.child(Constants.CHILD_USER_IMAGE)
+                                                .getValue()).toString();
                                 Picasso.get().load(mUserImage).placeholder(R.drawable.profile_image).into(mCurrentUserImage);
                             }
+                            Log.i(TAG, "getUserNameAndImage:onDataChange");
                         }
                     }
 
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
-                        Log.e(TAG, "addValueEventListener:onCancelled", error.toException());
+                        Log.e(TAG, "getUserNameAndImage:onCancelled", error.toException());
                     }
                 });
     }
@@ -103,45 +121,75 @@ public class OnlineActivity extends AppCompatActivity {
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.fragment_container, fragment);
+        transaction.replace(R.id.fragment_placeholder, fragment);
         transaction.commit();
     }
 
+    // Initializes views from layout
     private void initializeViews() {
-        mTabLayout = findViewById(R.id.online_tabLayout);
-        mContainer = findViewById(R.id.fragment_container);
-
-        // Setting AppBar
+        // Initializing AppBar
         mToolbar = findViewById(R.id.online_topAppBar);
-        mToolbar.getMenu().add(Menu.NONE, Constants.OFFLINE_MENU_ID, 1, R.string.menu_title_offline);
         mCurrentUserImage = findViewById(R.id.online_imageView);
         mTitleTextView = findViewById(R.id.online_title);
+
+        mTabLayout = findViewById(R.id.online_tabLayout);
     }
 
+    // Sets actions for tab layout and toolbar menu
+    private void setActions() {
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                if (position == 0) {
+                    replaceFragment(mContactsFragment);
+                } else if (position == 1) {
+                    replaceFragment(mExploreFragment);
+                }
+            }
 
-    private void setToolbarAction() {
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
         mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 switch (item.getItemId()) {
-                    case Constants.OFFLINE_MENU_ID:
-                        Toast.makeText(OnlineActivity.this, "Go offline selected", Toast.LENGTH_SHORT).show();
+                    case R.id.online_my_profile:
+                        goToProfileActivity();
+                        break;
+                    case R.id.online_go_offline:
                         goToOfflineActivity();
                         break;
-                    case R.id.item_settings:
-                        Toast.makeText(OnlineActivity.this, "Settings selected", Toast.LENGTH_SHORT).show();
+                    case R.id.online_settings:
                         goToSettingsActivity();
                         break;
-                    case R.id.item_help:
-                        Toast.makeText(OnlineActivity.this, "Help selected", Toast.LENGTH_SHORT).show();
+                    case R.id.online_help:
                         goToHelpActivity();
                         break;
-                    case R.id.item_logout:
-                        Toast.makeText(OnlineActivity.this, "Logout selected", Toast.LENGTH_SHORT).show();
-                        logoutUser();
+                    case R.id.online_logout:
+                        new MaterialAlertDialogBuilder(OnlineActivity.this)
+                                .setTitle(R.string.title_text_confirmation)
+                                .setMessage(R.string.message_logout_confirmation)
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .setPositiveButton(R.string.button_text_yes, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        logoutUser();
+                                    }
+                                })
+                                .setNeutralButton(R.string.button_text_no, null)
+                                .show();
                         break;
-                    case R.id.item_about:
-                        Toast.makeText(OnlineActivity.this, "About us selected", Toast.LENGTH_SHORT).show();
+                    case R.id.online_about:
                         goToAboutActivity();
                         break;
                     default:
@@ -152,52 +200,12 @@ public class OnlineActivity extends AppCompatActivity {
         });
     }
 
-    private void setTabLayoutAction() {
-        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                int position = tab.getPosition();
-                if (position == 0) {
-                    replaceFragment(mContactsFragment);
-                    System.out.println("Selected my contacts");
-                } else if (position == 1) {
-                    replaceFragment(mExploreFragment);
-                    System.out.println("Selected explore");
-                }
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-                int position = tab.getPosition();
-                if (position == 0) {
-                    System.out.println("Unselected my contacts");
-                } else if (position == 1) {
-                    System.out.println("Unselected explore");
-                }
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-                int position = tab.getPosition();
-                if (position == 0) {
-                    System.out.println("Reselected my contacts");
-                } else if (position == 1) {
-                    System.out.println("Reselected explore");
-                }
-            }
-        });
-    }
-
-    // For going back to Sign In
-    private void goToLogin() {
-        Intent intent = new Intent(this, LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-        finish();
-    }
-
+    /**
+     * This methods logs out user and clears any login info if saved.
+     * Then it calls {@link OnlineActivity#goToLogin()}.
+     */
     private void logoutUser() {
-        // forget login info
+        // Clear login info
         SharedPreferences preferences = getSharedPreferences(Constants.PREFERENCE_FILE_NAME, MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
         editor.putBoolean(Constants.REMEMBER_ME, false);
@@ -206,28 +214,59 @@ public class OnlineActivity extends AppCompatActivity {
         editor.apply();
         mAuth.signOut();
 
-        Log.i(TAG, "Login data cleared");
+        Log.i(TAG, "logoutUser:success");
         goToLogin();
     }
 
+    /**
+     * For going from this activity, {@link OnlineActivity}, to {@link LoginActivity}.
+     */
+    private void goToLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * For going from this activity, {@link OnlineActivity}, to {@link ProfileActivity}.
+     */
+    private void goToProfileActivity() {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * For going from this activity, {@link OnlineActivity}, to {@link OfflineActivity}.
+     */
     private void goToOfflineActivity() {
         Intent intent = new Intent(this, OfflineActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * For going from this activity, {@link OnlineActivity}, to {@link SettingsActivity}.
+     */
     private void goToSettingsActivity() {
         Intent intent = new Intent(this, SettingsActivity.class);
+        intent.putExtra(Constants.CONNECTION_FLAG, Constants.CONNECTION_ONLINE);
         startActivity(intent);
     }
 
+    /**
+     * For going from this activity, {@link OnlineActivity}, to {@link HelpActivity}.
+     */
     private void goToHelpActivity() {
-//        Intent intent = new Intent(this, OfflineActivity.class);
-//        startActivity(intent);
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
     }
 
+    /**
+     * For going from this activity, {@link OnlineActivity}, to {@link AboutActivity}.
+     */
     private void goToAboutActivity() {
-//        Intent intent = new Intent(this, OfflineActivity.class);
-//        startActivity(intent);
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
     }
 
 }
